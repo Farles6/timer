5ba1cd2f918ab961cbe07162dbd35e8de9b8a2b9
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-03-15 14:37:55 -0400

    refactored a little bit

--- timer1.js
+++ timer1.js
@@ -1,6 +1,6 @@
 let argv = process.argv.slice(2);
-for (let i = 0; i < argv.length; i++) {
-  let timer = Number(argv[i]) * 1000;
+for (let nums of argv) {
+  let timer = Number(nums) * 1000;
   if (timer < 0 || isNaN(timer)) {
     continue;
   }
